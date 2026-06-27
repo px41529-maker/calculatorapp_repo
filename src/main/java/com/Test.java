835f21e4f6394e3890bb5a22ee3ff91b1d51d4ca
px41529-maker <px41529@gmail.com>
2026-06-27 19:33:07 +0530

    Update Test.java

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -1,10 +1,5 @@
 package com;
 
 public class Test {
-	    private static final String PASSWORD = "admin123";
-
-    public static void main(String[] args) {
-        System.out.println("Password: " + PASSWORD);
-    }
-
+	
 }
